019f6040233b1863bdad575eaa691663892c1d9f
seylatann <69602888+seylatann@users.noreply.github.com>
2020-10-19 10:47:10 -0700

    Add files via upload

---
+++ classes/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace classes
+{
+    public class Transaction
+    {
+        public decimal Amount { get; }
+        public DateTime Date { get; set; }
+        public string Notes { get; }
+
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+}
